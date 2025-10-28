014d3109edcb77783db6e818a1fe855cf58d81a7
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-10-28 17:59:33 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Layout/Header";
 
-function App() {
-  return <h1 className="text-4xl text-purple-400 font-bold">Hello World!</h1>;
-}
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="bg-black/50">
+      <div className="text-2xl font-bold py-6 px-8">
+        <span className="inline-block bg-gradient-to-r from-yellow-200 to-yellow-500 bg-clip-text text-transparent">
+          {"{ ; }"} JSON Tree Visualizer
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Header;
